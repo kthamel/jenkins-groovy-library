3491a3343bbbbe85675cf1d90b3f64f5a9a71829
Kushan Thamel <kushan.thamel@hotmail.com>
2023-08-09 13:42:20 +0530

    groovy errors were fixed

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,6 @@
-def call(String name,String dayOfWeek) {
+/* groovylint-disable MethodReturnTypeRequired */
+/* groovylint-disable-next-line MethodReturnTypeRequired */
+/* groovylint-disable-next-line NoDef */
+def call(String name, String dayOfWeek) {
     sh "echo Hi ${name}, Today is ${dayOfWeek}"
 }
